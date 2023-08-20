bdb5eff03183e264ce47842537af97b9172dd106
Damirka1 <hamitov.damir3101@icloud.com>
2023-08-20 21:08:05 +0600

    Set up notification center with track controls and info

--- Damirka.Space/Services/PlayerService.swift
+++ Damirka.Space/Services/PlayerService.swift
@@ -7,6 +7,7 @@
 
 import AVKit
 import Combine
+import MediaPlayer
 
 final class PlayerService : AVPlayer, ObservableObject {
     private var albumId = -1
@@ -20,7 +21,11 @@ final class PlayerService : AVPlayer, ObservableObject {
    
     private var timeObserverToken: Any?
     
+    private var nowPlayingInfo = [String: Any]()
+    
     @Published var currentTimeInSeconds: Double = 0.0
+    
+    var observer: NSKeyValueObservation?
     
     var currentTimeInSecondsPass: AnyPublisher<Double, Never>  {
         return $currentTimeInSeconds
@@ -37,6 +42,16 @@ final class PlayerService : AVPlayer, ObservableObject {
         timeObserverToken = self.addPeriodicTimeObserver(forInterval: interval, queue: .main) {
             [weak self] _ in
             self?.currentTimeInSeconds = self?.currentTime().seconds ?? 0.0
+            
+            self?.nowPlayingInfo[MPNowPlayingInfoPropertyElapsedPlaybackTime] = CMTimeGetSeconds((self?.currentTime())!)
+            
+            if(self?.isPlaying() == true) {
+                self?.nowPlayingInfo[MPNowPlayingInfoPropertyPlaybackRate] = 1
+            } else {
+                self?.nowPlayingInfo[MPNowPlayingInfoPropertyPlaybackRate] = 0
+            }
+            
+            MPNowPlayingInfoCenter.default().nowPlayingInfo = self?.nowPlayingInfo
         }
     }
     
@@ -82,6 +97,92 @@ final class PlayerService : AVPlayer, ObservableObject {
         self.albumId = id
         showBarView = true
     }
+    
+    
+    func setupNotificationView() {
+        let track = tracks![currentIndex]
+        
+        guard let url = URL(string: track.metadataImageUrl) else {
+            return
+        }
+        
+        let request = URLRequest(url: url)
+        
+        let task = URLSession.shared.dataTask(with: request) { [self] data, response, error in
+            if error != nil {
+                // Handle error
+                return
+            }
+            
+            guard let data = data else {
+                return
+            }
+            
+            nowPlayingInfo = [String: Any]()
+            
+            nowPlayingInfo[MPMediaItemPropertyTitle] = track.title
+            
+            nowPlayingInfo[MPMediaItemPropertyAlbumTitle] = track.album
+            
+            nowPlayingInfo[MPMediaItemPropertyArtist] = track.author.joined(separator: ", ")
+            
+            nowPlayingInfo[MPMediaItemPropertyPlaybackDuration] = CMTimeGetSeconds(currentItem!.duration)
+            
+            guard let img = UIImage(data: data) else {
+                MPNowPlayingInfoCenter.default().nowPlayingInfo = nowPlayingInfo
+                return
+            }
+            
+            nowPlayingInfo[MPMediaItemPropertyArtwork] = MPMediaItemArtwork(boundsSize: img.size, requestHandler: { (size) -> UIImage in
+                return img
+            })
+            
+            MPNowPlayingInfoCenter.default().nowPlayingInfo = nowPlayingInfo
+        }
+        task.resume()
+    }
+    
+    func setupMediaPlayerNotificationView() {
+        do {
+            NotificationCenter.default.addObserver(forName: NSNotification.Name.AVPlayerItemDidPlayToEndTime, object: nil, queue: nil) { [self] notification in
+                playTrack(track: currentIndex + 1)
+            }
+            
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback)
+            let _ = try AVAudioSession.sharedInstance().setActive(true)
+            
+            let commandCenter = MPRemoteCommandCenter.shared()
+            
+            commandCenter.playCommand.addTarget {
+                [unowned self] event in
+                self.play()
+                return .success
+            }
+            
+            commandCenter.pauseCommand.addTarget {
+                [unowned self] event in
+                self.pause()
+                return .success
+            }
+            
+            commandCenter.previousTrackCommand.addTarget {
+                [unowned self] event in
+                self.playPrev()
+                return .success
+            }
+
+            commandCenter.nextTrackCommand.addTarget {
+                [unowned self] event in
+                self.playNext()
+                return .success
+            }
+            
+            setupNotificationView()
+            
+        } catch let error as NSError {
+            print("an error occurred when audio session category.\n \(error)")
+        }
+    }
 
     
     func playTrack(track: Int) {
@@ -95,24 +196,19 @@ final class PlayerService : AVPlayer, ObservableObject {
             currentIndex = track
         }
         
-        do {
-            NotificationCenter.default.addObserver(forName: NSNotification.Name.AVPlayerItemDidPlayToEndTime, object: nil, queue: nil) { [self] notification in
-                playTrack(track: currentIndex + 1)
-            }
-            
-            try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback)
-            let _ = try AVAudioSession.sharedInstance().setActive(true)
-        } catch let error as NSError {
-            print("an error occurred when audio session category.\n \(error)")
-        }
+        pause()
         
         let playerItem = AVPlayerItem.init(url: URL(string: tracks![currentIndex].url)!)
         
-        pause()
-        
         self.replaceCurrentItem(with: playerItem)
         
-        play()
+        // Register as an observer of the player item's status property
+        self.observer = playerItem.observe(\.status, options:  [.new, .old], changeHandler: { [self] (playerItem, change) in
+            if playerItem.status == .readyToPlay {
+                setupMediaPlayerNotificationView();
+                play()
+            }
+        })
     }
     
     func playNext() {
